7fa0327ddf1aab743c28b306a7724130edc2df9d
Tomas Baca <Tomas Baca>
2025-06-10 21:53:41 +0200

    not deleting the workspace tar

--- scripts/ros_test/run_test.sh
+++ scripts/ros_test/run_test.sh
@@ -83,8 +83,6 @@ if [ -e $ARTIFACTS_FOLDER/workspace.tar.gz ]; then
 
   tar -xvzf $ARTIFACTS_FOLDER/workspace.tar.gz -C /tmp/
 
-  rm $ARTIFACTS_FOLDER/workspace.tar.gz
-
 else
 
   echo "$0: creating the workspace"
